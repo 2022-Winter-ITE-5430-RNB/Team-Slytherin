8f07fc685c3851bfda8152498ad18410ed6837bf
Srinath Patnam <N01495266@humbermail.ca>
2022-04-18 20:43:59 -0400

    Event details

--- src/Components/Events/AEventDetails.js
+++ src/Components/Events/AEventDetails.js
@@ -12,7 +12,7 @@ import DelEvent from './DelEvent'
 
 const AEventDetails = (props) => {
   let eventid = localStorage.getItem('eventid')
-   
+
   const navigate = useNavigate();
   const [showEdit, setShowEdit] = useState(false)
   const editClicked = () => {
@@ -22,7 +22,7 @@ const AEventDetails = (props) => {
     console.log({ showEdit });
   }
   const eventDetails = useState(props.event)
-  const [showDel,setShowDel]=useState('false')
+  const [showDel, setShowDel] = useState('false')
   const [open, setOpen] = useState(false);
 
   return (
@@ -31,54 +31,54 @@ const AEventDetails = (props) => {
       <ButtonToolbar aria-label="Action on the events">
         {
           showEdit ?
-          <ButtonGroup className="me-2" aria-label="Edit Event">
+            <ButtonGroup className="me-2" aria-label="Edit Event">
               <Button onClick={editClicked}>Cancel</Button>
             </ButtonGroup> :
             <ButtonGroup className="me-2" aria-label="Edit Event">
               <Button onClick={editClicked}>Edit</Button>
             </ButtonGroup>
-            
+
         }
 
-        <DelEvent key ={eventid} ></DelEvent> 
-       
+        <DelEvent key={eventid} ></DelEvent>
+
       </ButtonToolbar>
-     
+
       <>
-      <Container>
+        <Container>
           <Row>
             <Col>
-              
+
               {
-               
-               eventDetails.map((event,index) => {
-                 console.log(event.title);
-                 let url = 'http://localhost:5000/uploads/'
-                 const eventPic = !!(event.eImage)?url+event.eImage:dogImg;
-                 return(
-                   index===0?                
-                 
-                     <img src={eventPic}  alt={event.title} />:
-                  
-                   <div></div>
-                 ) 
-               })}
+
+                eventDetails.map((event, index) => {
+                  console.log(event.title);
+                  let url = 'http://localhost:5000/uploads/'
+                  const eventPic = !!(event.eImage) ? url + event.eImage : dogImg;
+                  return (
+                    index === 0 ?
+
+                      <img src={eventPic} alt={event.title} /> :
+
+                      <div></div>
+                  )
+                })}
 
             </Col>
-            
+
             <Col>
               {
-               
-                eventDetails.map((event,index) => {
+
+                eventDetails.map((event, index) => {
                   console.log(event.title);
-                  
-                  return(
-                    index===0?                
-                    <Event1 event={event} id={event._id} show={showEdit}  />:
-                    <div></div>
-                    
+
+                  return (
+                    index === 0 ?
+                      <Event1 event={event} id={event._id} show={showEdit} /> :
+                      <div></div>
+
                   )
-                  
+
                 })}
             </Col>
           </Row>
